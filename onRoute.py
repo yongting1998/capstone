5cd8cc2e3051311a5b8d0f8e2c32de9f642cd202
yongting1998 <32158771+yongting1998@users.noreply.github.com>
2023-01-15 23:08:00 +0800

    Update onRoute.py

--- onRoute.py
+++ onRoute.py
@@ -11,23 +11,26 @@ LarkinToKulaiData = json.load(fLarkinToKulai)
 
 colNames=['id', 'trip_id', 'bus_line_id', 'socket_date', 'socket_datetime', 'lat', 'long', 'bus_vehicle_id', 'bus_plate', 'station_id', 'station_code', 'distance', 'speed', 'bearing', 'status','created', 'updated','direction']
 
-df = pd.read_csv ('./dataset_direction/202105_direction.csv', names=colNames, skiprows=1)
+df = pd.read_csv ('./dataset_direction/202202_direction.csv', names=colNames, skiprows=1)
 
 df = df.reset_index()
 for index, row in df.iterrows():
     print(index)
+    check = False
     if row["direction"] == 1:
         for point in KulaiToLarkinData:
             if(abs(point[0] - row["lat"]) < 1e-4 and abs(point[1] - row["long"]) < 1e-4):
-                df.drop(index, inplace=True)
+                check = True
                 break
     elif row["direction"] == 2:
         for point in LarkinToKulaiData:
             if(abs(point[0] - row["lat"]) < 1e-4 and abs(point[1] - row["long"]) < 1e-4):
-                df.drop(index, inplace=True)
+                check = True
                 break
-
-df.to_csv('./dataset_onRoute/202105_onRoute.csv')
+    if not check:
+        df.drop(index, inplace=True)
+        
+df.to_csv('./dataset_onRoute/202202_onRoute.csv')
 
 plt.scatter(x=df['lat'], y=df['long'])
 plt.show()
