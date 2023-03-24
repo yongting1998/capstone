c614b132c3e0df400f65cc012893d12253dde553
yongting1998 <32158771+yongting1998@users.noreply.github.com>
2023-03-24 21:01:44 +0800

    clean

--- part_A/part_A_pre_processing.py
+++ part_A/part_A_pre_processing.py
@@ -2,6 +2,12 @@ import pandas as pd
 from datetime import date
 import datetime as dt
 import matplotlib.pyplot as plt
+import json
+
+fKulaiToLarkin = open('../dataset/KulaiToLarkin.json')
+KulaiToLarkinData = json.load(fKulaiToLarkin)
+fLarkinToKulai = open('../dataset/LarkinToKulai.json')
+LarkinToKulaiData = json.load(fLarkinToKulai)
 
 def timeTaken():
     colNames=['index','socket_date','socket_datetime','lat','long','distance','speed','direction','busStop']
@@ -111,9 +117,32 @@ def minute():
         df['minuteOfDay'] = (df['socket_datetime'].dt.hour) * 60 + df['socket_datetime'].dt.minute
 
         df.to_csv('../dataset/part_A/dataset_minute/' + x  + '_minute.csv', index=False)
+        df.to_csv('../dataset/part_A/dataset_minute/full_minute.csv', index=False, mode='a')
+
+def distance():
+    dataNames=['202105','202106', '202107', '202108', '202109', '202110', '202111', '202112', '202201', '202202', '202203']
+    colNames=['socket_date', 'socket_datetime', 'lat', 'long', 'distance', 'speed', 'direction', 'busStop', 'time_taken','day_of_week','minuteOfDay']
+    for x in dataNames:
+        df = pd.read_csv ('../dataset/part_A/dataset_minute/' + x + '_minute.csv', names=colNames, skiprows=1)
+        df = df.reset_index()
+        for index, row in df.iterrows():
+            print(index)
+            if row["direction"] == 1:
+                for point in KulaiToLarkinData['stops']:
+                    if row['busStop'] == point['code']:
+                        df.at[index, 'distanceToNext'] = point['distanceToNext']
+                        break
+            elif row["direction"] == 2:
+                for point in LarkinToKulaiData['stops']:
+                    if row['busStop'] == point['code']:
+                        df.at[index, 'distanceToNext'] = point['distanceToNext']
+                        break
+
+        df.to_csv('../dataset/part_A/dataset_distance/' + x + '_distance.csv', index=False)
 
 
 timeTaken()
 outlier()
 timeOfDay()
 minute()
+# distance()
